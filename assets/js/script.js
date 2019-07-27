47aa416a56c2078e579199d40f8d255118402316
hosti <whitevipx@gmail.com>
2019-07-27 18:10:19 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+
+
+// console.log (letBorderWidth);
+
+
+var letBorderWidth = 1;
+let second =2;
+const pi = 3.14;
+
+// console.log (letBorderWidth); 
+
+var number = 5;
+var string = 'hello';
+var symbol = Symbol ();
+var boolean = true;
+null; 
+undefined;
+
+var obj = {};
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+// console.log(persone.name);
+
+console.log(persone[ "name", "age" ]);
+
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[2]);
+
+// alert ('weqweasd');
+
+// let answer = confirm('Are you here?');
+// console.log(answer);
+
+// let answer = prompt('Are you 18?', 'Yes');
+// console.log(typeof(arr));
+
+// console.log('arr' + ' - object'); //string
+// console.log(4 + ' - object'); //string
+// console.log(4 + +' - object'); // nan - number
+// console.log(4 + 7); //number
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+console.log (incr);
+console.log (decr);
+
+let isChecked = true,
+    isClose = false;
+    console.log(isChecked && isClose); // operator and (i)
+    console.log(isChecked || isClose); // oreraror or (ili)
+    console.log(isChecked || !isClose);
+
+
+
