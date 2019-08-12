d1fbc174db5c0d8c632437f7f6abb538af91036a
hosti <whitevipx@gmail.com>
2019-08-12 21:47:47 +0300

    new commit

--- assets/js/js1.js
+++ assets/js/js1.js
@@ -1,30 +1,113 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?'); //budget
-let time = prompt("Введите дату в формате", "YYYY-MM-DD"); // date
+let money, time;
 
+function start () {
+    money = +prompt('Ваш бюджет на месяц?', ""); //budget
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""); // date
 
-
-
-let expenseOne = prompt('Введите обязательную статью расходов в этом месяце', 'Введите обязательную статью расходов в этом месяце');
-let expenseTwo = prompt  ('Во сколько обойдется?', 'Во сколько обойдется?');
-
-
-let expenses = {  //  expenses: {“ответ на первый вопрос” : “ответ на второй вопрос”}
-    first: expenseOne,
-    second: expenseTwo
-
-};
-console.log(expenses);
-let optionalExpenses = {
-};
+    while (isNaN(money) || money == '' || money==null) {
+        money = +prompt('Ваш бюджет на месяц?', "");
+    }
+}
+start ();
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: expenses,
-    optionalExpenses: optionalExpenses,
+    expenses: {},
+    optionalExpenses: {},
     income: [],
-    savings: false,
+    savings: true,
 };
-alert (money/30);
+
+// for ( let i=0; i < 2; i++ ){ // создаем цыкл со счетчиком в 2 раза
+//     let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//         b = prompt  ('Во сколько обойдется?', '');
+//         if ( (typeof(a)) === 'string' && (typeof (a)) !=null && (typeof (b)) !=null 
+//         && a !='' && b !='' && a.length<50 && b.length<50) {
+//             console.log('done');
+//             appData.expenses[a] = b; // создаем обьект и пару --- ключ = значение в обьекте
+//         } else {
+//             i = i-1;
+//         }   
+// };
+
+function chooseExpenses () {
+    for ( let i=0; i < 2; i++ ){ // создаем цыкл со счетчиком в 2 раза
+             let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+                b = prompt  ('Во сколько обойдется?', '');
+                if ( (typeof(a)) === 'string' && (typeof (a)) !=null && (typeof (b)) !=null 
+                && a !='' && b !='' && a.length<50 && b.length<50) {
+                    console.log('done');
+                   appData.expenses[a] = b; // создаем обьект и пару --- ключ = значение в обьекте
+               } else {
+                    i = i-1;
+               }   
+        };
+}
+chooseExpenses();
+
+
+
+
+/*
+let i = 0;
+while (i < 2 ){
+    i++;
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt  ('Во сколько обойдется?', '');
+        if ( (typeof(a)) === 'string' && (typeof (a)) !=null && (typeof (b)) !=null 
+        && a !='' && b !='' && a.length<50 && b.length<50) {
+            console.log('done');
+            appData.expenses[a] = b; // создаем обьект и пару --- ключ = значение в обьекте
+        } else {
+        }   
+
+};*/
+/*
+let i = 0;
+do {
+  i++;
+  let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt  ('Во сколько обойдется?', '');
+        if ( (typeof(a)) === 'string' && (typeof (a)) !=null && (typeof (b)) !=null 
+        && a !='' && b !='' && a.length<50 && b.length<50) {
+            console.log('done');
+            appData.expenses[a] = b; // создаем обьект и пару --- ключ = значение в обьекте
+        } else {
+        }   
+} while (i < 2);
+*/
+
+appData.moneyPerDay = (appData.budget / 30).toFixed(2); //.toFixed - до целого числа если пусто, в нашем случае до второго после запятой - возвращает стринг
+
+alert ('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+
+if (appData.moneyPerDay < 100 ) {
+    console.log('it`s a minimum');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('it`s a medium');
+    } else if (appData.moneyPerDay > 2000) {
+    console.log('it`s a high');
+    } else {
+    console.log('Error'); 
+    }
+
+
+
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
+// console.log(arr[0]);
+// alert('helo world');
+// confirm ('ask');
+
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt ('Какова сумма накоплений?', ''),
+        percent = +prompt ('Под какой процент?', '');
+        appData.monthIncome = save/100/12*percent;
+        alert('Доход в месяц с депозита:' + appData.monthIncome);
+    }
+}
+checkSavings();
--- assets/js/script.js
+++ assets/js/script.js
@@ -47,16 +47,16 @@ console.log(arr[2]);
 // console.log(4 + ' - object'); //string
 // console.log(4 + +' - object'); // nan - number
 // console.log(4 + 7); //number
-
+/*
 let incr = 10,
     decr = 10;
 
-    incr++;
-    decr--;
+    incr++; //увеличит на 1
+    decr--; //уменьшит на 1
 
-console.log (incr);
+console.log (incr); //если плюс до ++incr то выведется 11 если после то вывод будет 10 но после добавится до 11
 console.log (decr);
-
+*/
 let isChecked = true,
     isClose = false;
     console.log(isChecked && isClose); // operator and (i)
@@ -82,6 +82,8 @@ if (num < 49) {
 
 (num == 50) ? console.log('enough') : console.log('incorrectly');
 
+
+let num = 50;
 switch (num) {
     case num < 49: 
     console.log('enough');
@@ -99,3 +101,162 @@ switch (num) {
     console.log ('something wrong');
     break;
 }
+/*цыклы*/
+let three = 3,
+    two = "3";
+    console.log(three === two);
+
+    for (let i=1; i<=8; i++) {
+        console.log (i*i);
+    }
+
+
+    let num = 50;
+    // while (num < 55 ) {
+    //     console.log(num);
+    //     num++;
+    // }
+
+    do {
+        console.log(num);
+        num++;
+    }
+    while (num<55);
+
+    for (let i = 1; i < 8; i++){
+        if (i==6) {
+            continue;
+        }
+        console.log(i)
+    }
+
+    let x = 5; console.log( x++ ); 
+    let y = [ ] + false - null + true;  console.log( y );
+    let y = 1; let x = y = 2; console.log(x);
+    let x = [ ] + 1 + 2; console.log(x); let y = typeof (x); console.log(y);
+    alert( "1"[0] ); console.log("1"[0]);
+    let x = 2 && 1 && null && 0 && undefined; console.log( x );
+    let a=1, b =2; 
+    let z = !!( a && b ); let x = (a && b);
+    z==x ? console.log('yes') : console.log('no');
+    console.log(z); console.log(x);
+
+    console.log( null || 2 && 3 || 4 ); 	
+    
+    let a = [1, 2, 3], b = [1, 2, 3];
+    if (a==b){
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
+    console.log( +"Infinity" );
+
+let a = "яблоко", g ="ёжик";
+    if (g > a) {
+        console.log('yes');
+} else {
+    console.log('no');
+}
+
+console.log(a);
+console.log(b);
+
+let x = 0 || "" || 2 || undefined || true || false ;
+console.log (x);
+
+let y = 0 || "" || 0 || 0 || 9 && 1  || 6  || 7  || 8 ;
+console.log (y);
+
+
+let y = "";
+console.log (y);
+
+let num = 20;
+function showFirstMessage(text) { // функция, имя функции для того чтоб вызвать ее в будущем (если имени нет - значит анонимная и тут и сейчас используется)
+    // далее аргумент - (text) - может быть сколько угодно, нужны для того чтоб функция с ними работала
+    console.log(text);
+    let num = 15; // переменная внутри функции - локальная и не видна далее
+    console.log  (num); // 
+
+showFirstMessage('lol'); // вызвали функцию, с одним аргументом
+console.log  (num);
+
+
+
+function z(a) {
+    let a = 1;
+    return function (a) {
+    return a++;
+   }
+}
+let a = z(3);
+console.log (a) ;
+
+
+/* //пример замыкания
+let c = 5;
+function addX(x) {
+  return function(n) {
+    return n + x;
+  }
+}
+const addThree = addX(3);
+let d = addThree(c);
+console.log('пример фиксации аргумента:', d);*/
+
+
+function makeShout() { // (1)
+        var phrase = "Превед!"  // (2)
+        var shout = function() {  // (3,4)
+            console.log(phrase)
+        }
+        phrase = "Готово!"  // (5)
+        return shout
+    }
+    shout = makeShout()
+    // что выдаст?
+    shout();
+
+
+    function addHideHandler(sourceId, targetId) {
+        // создан объект [[scope]] со свойствами sourceId, targetId
+    
+        // записать в [[scope]] свойство sourceNode
+        var sourceNode = document.getElementById(sourceId)
+    
+        // записать в [[scope]] свойство handler
+        var handler = function() {
+            var targetNode = document.getElementById(targetId)
+            targetNode.style.display = ‘none’
+        }
+    
+        sourceNode.onclick = handler
+    
+        // функция закончила выполнение
+        // (***) и тут - самое интересное!
+    }
+
+
+    // console.log(calc(3,4));
+    // console.log(calc(4,7));
+    // function calc (a,b) { //калькулятор
+    //     return (a + b);
+    // }
+    // console.log(calc(3,4));
+    // console.log(calc(4,7));
+
+    let calc = function  (a,b) { //калькулятор но работает только ПОсле того как код до него дошел а функция работает до обьявления
+    return (a + b);
+    }
+    console.log(calc(3,4));
+    console.log(calc(4,7));
+
+
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
